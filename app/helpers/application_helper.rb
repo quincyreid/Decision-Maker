8a3a5c496c1aedff63dc09decebc98c92136bf08
quincyreid <qreid51@gmail.com>
2012-06-29 02:40:39 -0400

    updated percentage method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-
+  
   def percentage_of_votes(candidate)
-            ((((Vote.find_all_by_voteable_id(candidate.id)).length)/(Vote.count.to_f))*100).to_s+'%'
+    ((((Vote.find_all_by_voteable_id(candidate.id).length)/(Vote.count.to_f))*100).to_s+'%')
   end
-
-
+  
+  
 end
--- app/helpers/candidate_helper.rb
+++ app/helpers/candidate_helper.rb
@@ -1,13 +1,3 @@
 module CandidateHelper
-  def max_vote_count(candidates)
-    count = []
-    
-    candidates.each do |c|
-      count << c.votes_count
-    end
-    
-    if count.max == 1 then vote = "vote" else vote = "votes" end
-      @winner_points = "Current Winner has: #{count.max} #{vote}"
-    end
-  
+
 end
