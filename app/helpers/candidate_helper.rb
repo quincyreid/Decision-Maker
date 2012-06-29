c6d10ee1427bc113e5bdc61e8bc25fb30b583940
quincyreid <qreid51@gmail.com>
2012-06-29 02:02:47 -0400

    removed total_count method

--- app/helpers/candidate_helper.rb
+++ app/helpers/candidate_helper.rb
@@ -9,14 +9,5 @@ module CandidateHelper
     if count.max == 1 then vote = "vote" else vote = "votes" end
       @winner_points = "Current Winner has: #{count.max} #{vote}"
     end
-    
-    def total_votes(candidates)
-      count = 0
-      
-      candidates.each do |c|
-        count += c.votes_count
-      end
-      count
-    end
   
 end
